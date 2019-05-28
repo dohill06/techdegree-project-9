c1a1e166bcd270a2b2d5c7cbb1883f2259423e89
Douglas Hill <dohill06@gmail.com>
2019-05-28 12:29:07 -0700

    Require sequelize

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 // load modules
 const express = require('express');
 const morgan = require('morgan');
+const sequelize = require('./models').sequelize;
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
