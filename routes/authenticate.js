ad5333449227bc4eaa0fb75d0486668cefcabece
Douglas Hill <dohill06@gmail.com>
2019-05-29 12:09:43 -0700

    Update authenticate.js

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -1,9 +1,25 @@
 const bcryptjs = require('bcryptjs');
-const authenticate = require('basic-auth');
+const auth = require('basic-auth');
 const User = require('../models').User;
 
 
 module.exports = (req, res, next) => {
 
-    next();
-}
+    const credentials = auth(req);
+
+    if (credentials) {
+        User.findOne({where: {emailAddress: credentials.name}})
+        .then(user => {
+            if (user) {
+                const authenticated = bcryptjs
+                .compareSync(credentials.pass, user.password);
+                if (authenticated) {
+                    req.currentUser = user;
+                    next();
+                }
+            }
+        });
+    }
+
+    
+};
