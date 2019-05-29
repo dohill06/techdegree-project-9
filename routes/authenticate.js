7327796d4b951257b23472eed831be1170c93782
Douglas Hill <dohill06@gmail.com>
2019-05-29 12:48:21 -0700

    Update authenticate.js

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -4,22 +4,37 @@ const User = require('../models').User;
 
 
 module.exports = (req, res, next) => {
-
+    let message = null;
     const credentials = auth(req);
 
     if (credentials) {
-        User.findOne({where: {emailAddress: credentials.name}})
-        .then(user => {
-            if (user) {
-                const authenticated = bcryptjs
-                .compareSync(credentials.pass, user.password);
-                if (authenticated) {
-                    req.currentUser = user;
-                    next();
+        User.findOne({
+                where: {
+                    emailAddress: credentials.name
                 }
-            }
-        });
+            })
+            .then(user => {
+                if (user) {
+                    const authenticated = bcryptjs
+                        .compareSync(credentials.pass, user.password);
+                    if (authenticated) {
+                        req.currentUser = user;
+                    } else {
+                        message = 'Authentication Failure';
+                    }
+                } else {
+                    message = 'User Not Found';
+                }
+            });
+    } else {
+        message = 'Please Log In';
     }
 
-    
+    if (message) {
+        const err = new Error(message);
+        err.status = 401;
+        next(err);
+    } else {
+        next();
+    }
 };
