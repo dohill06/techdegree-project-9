9fc37156f1170f03e5fc09a614383a59fe5aadea
Douglas Hill <dohill06@gmail.com>
2019-05-29 11:38:16 -0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models').User;
+const authenticateUser = require('./authenticate');
+const bcryptjs = require('bcryptjs');
+
+router.get('/', authenticateUser, (req, res) => {
+
+});
 
 
 
