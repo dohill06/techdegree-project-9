dde0aaf0d6f19b2bb47f32ff940a7cda57aea7f0
Douglas Hill <dohill06@gmail.com>
2019-05-29 11:35:20 -0700

    Update authenticate.js

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -1,3 +1,9 @@
 const bcryptjs = require('bcryptjs');
 const authenticate = require('basic-auth');
 const User = require('../models').User;
+
+
+module.exports = (req, res, next) => {
+
+    next();
+}
