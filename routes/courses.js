12c98dec1e8a4918960338a89eb615b31224887d
Douglas Hill <dohill06@gmail.com>
2019-05-30 09:33:13 -0700

    Set up all routes for courses.js

--- routes/courses.js
+++ routes/courses.js
@@ -5,5 +5,24 @@ const User = require('../models').User;
 const authenticateUser = require('./authenticate');
 
 
+router.get('/', (req, res, next) => {
+
+});
+
+router.get('/:id', (req, res, next) => {
+
+});
+
+router.post('/', (req, res, next) => {
+
+});
+
+router.put('/:id', (req, res, next) => {
+
+});
+
+router.delete('/:id', (req, res, next) => {
+    
+});
 
 module.exports = router;
