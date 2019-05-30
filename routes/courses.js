5f7413a0cf5238d265e0fb1deba623e24f135a5f
Douglas Hill <dohill06@gmail.com>
2019-05-30 09:21:48 -0700

    Add requirements to courses.js

--- routes/courses.js
+++ routes/courses.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const Course = require('../models').Course;
+const User = require('../models').User;
+const authenticateUser = require('./authenticate');
 
 
 
