f14b525cf0577d789586baac7aea0e9898f3e08c
Douglas Hill <dohill06@gmail.com>
2019-05-30 09:10:08 -0700

    Update course.js

--- models/course.js
+++ models/course.js
@@ -1,40 +1,40 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Course = sequelize.define('Course', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    title: {
-        type: DataTypes.STRING,
-        validate: {
-            notEmpty: {
-                msg: 'Course title required'
+    const Course = sequelize.define('Course', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            validate: {
+                notEmpty: {
+                    msg: 'Course title required'
+                }
             }
-        }
-    },
-    description: {
-        type: DataTypes.TEXT,
-        validate: {
-            notEmpty: {
-                msg: 'Description required'
+        },
+        description: {
+            type: DataTypes.TEXT,
+            validate: {
+                notEmpty: {
+                    msg: 'Description required'
+                }
             }
+        },
+        estimatedTime: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        materialsNeeded: {
+            type: DataTypes.STRING,
+            allowNull: true
         }
-    },
-    estimatedTime: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },
-    materialsNeeded: {
-        type: DataTypes.STRING,
-        allowNull: true
-    }
-  }, {});
-  Course.associate = (models) => {
-    Course.belongsTo(models.User, {
-      foreignKey: 'userId'
-    });
-  };
-  return Course;
+    }, {});
+    Course.associate = (models) => {
+        Course.belongsTo(models.User, {
+            foreignKey: 'userId'
+        });
+    };
+    return Course;
 };
